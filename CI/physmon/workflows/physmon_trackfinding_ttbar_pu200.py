0859c30ffbec309749c598363b4892cabfb9800b
AJPfleger <alexander.julian.pfleger@cern.ch>
2024-10-02 17:24:48 +0200

    try 30 events

--- CI/physmon/workflows/physmon_trackfinding_ttbar_pu200.py
+++ CI/physmon/workflows/physmon_trackfinding_ttbar_pu200.py
@@ -35,7 +35,7 @@ setup = makeSetup()
 
 with tempfile.TemporaryDirectory() as temp:
     s = acts.examples.Sequencer(
-        events=3,
+        events=30,
         numThreads=-1,
         logLevel=acts.logging.INFO,
     )
